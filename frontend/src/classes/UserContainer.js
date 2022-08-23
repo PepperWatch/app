d3d339e4955546d13096d267bf481f416f643d85
pprwj <j@pepperwatch.com>
2022-08-23 03:40:06 +0300

    collection in json

--- frontend/src/classes/UserContainer.js
+++ frontend/src/classes/UserContainer.js
@@ -69,10 +69,10 @@ export default class UserContainer {
 			"external_url": "https://pepperwatch.com/v/"+this.id,
 			"image": (""+this.getPublicThumbIPFSHashURL()),
 			"animation_url": (""+this.getIPFSHashURL()),
-			"collection":{
-				"name":"PepperWatch Master",
-				"family":"PepperWatch Master",
-			},
+			// "collection":{
+			// 	"name":"PepperWatch Master",
+			// 	"family":"PepperWatch Master",
+			// },
 			"attributes": [
 
 			],
